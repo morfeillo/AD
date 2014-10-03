44e5031a8c64f34365da8fce2e44b8f9c2b61a98
morfeillo <morfeillo@gmail.com>
2014-10-03 12:12:45 +0200

    messagedialog y mas cosas

--- PCategoria/PCategoria/MainWindow.cs
+++ PCategoria/PCategoria/MainWindow.cs
@@ -41,12 +41,29 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		deleteAction.Sensitive = false;
 		Cargar ();
 		//Añadir columnas, si no hay datos no sale, pero la columna está añadida
 		treeView.AppendColumn ("id", new CellRendererText (), "text", 0); // nombre, tipoque vamos a dibujar, tipo texto, columna index
 		treeView.AppendColumn ("nombre", new CellRendererText (), "text", 1);
+
+		//forma 1 de usar eventos.
+		//treeView.Selection.Changed += selectionChanged;
+		//forma 2 usando delegate
+		treeView.Selection.Changed += delegate {
+			//se sustiye los ifs, es una expresion booleana que devuelve true o false.
+			deleteAction.Sensitive = treeView.Selection.CountSelectedRows() > 0;
+
+	};
 	}
 
+
+
+	/*private void selectionChanged (object sender, EventArgs e){
+		Console.WriteLine ("selectionChanged");
+		deleteAction.Sensitive = true;
+	}
+	*/
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
@@ -76,6 +93,45 @@ public partial class MainWindow: Gtk.Window
 	{
 		listStore.Clear ();
 		Cargar ();
+
+	}
+
+	protected void OnDeleteActionActivated (object sender, EventArgs e)
+	{
+		TreeIter treeIter;
+		treeView.Selection.GetSelected(out treeIter);
+		object id = listStore.GetValue(treeIter,0);
+		//object nombre = listStore.GetValue (treeIter, 1);
+		if (!Confirm("¿Quieres eliminar el registro?"))
+		    return;
+
+		}
+
+
+		public bool Confirm (string text){
+
+		MessageDialog messageDialog = new MessageDialog (
+			this,
+			DialogFlags.Modal,
+			MessageType.Question, ButtonsType.YesNo, "¿Quieres eliminarlo?");
+
+		messageDialog.Run ();
+
+		ResponseType response = (ResponseType)messageDialog.Run ();
+
+		if ((ResponseType)messageDialog.Run () == ResponseType.Yes) {
+			messageDialog.Destroy ();
+			mySqlCommand.CommandText = string.Format ("delete from categoria where id={0}", id); 
+			mySqlCommand.ExecuteNonQuery ();
+			listStore.Clear ();
+			Cargar ();
+
+
+		} if ((ResponseType)messageDialog.Run () == ResponseType.No) {
+
+			messageDialog.Destroy ();
+			return;
+		}
 
 	}
 
--- PCategoria/PCategoria/gtk-gui/MainWindow.cs
+++ PCategoria/PCategoria/gtk-gui/MainWindow.cs
@@ -5,6 +5,7 @@ public partial class MainWindow
 	private global::Gtk.UIManager UIManager;
 	private global::Gtk.Action addAction;
 	private global::Gtk.Action refreshAction;
+	private global::Gtk.Action deleteAction;
 	private global::Gtk.VBox vbox1;
 	private global::Gtk.Toolbar toolbar1;
 	private global::Gtk.ScrolledWindow GtkScrolledWindow;
@@ -20,6 +21,8 @@ public partial class MainWindow
 		w1.Add (this.addAction, null);
 		this.refreshAction = new global::Gtk.Action ("refreshAction", null, null, "gtk-refresh");
 		w1.Add (this.refreshAction, null);
+		this.deleteAction = new global::Gtk.Action ("deleteAction", null, null, "gtk-delete");
+		w1.Add (this.deleteAction, null);
 		this.UIManager.InsertActionGroup (w1, 0);
 		this.AddAccelGroup (this.UIManager.AccelGroup);
 		this.Name = "MainWindow";
@@ -30,7 +33,7 @@ public partial class MainWindow
 		this.vbox1.Name = "vbox1";
 		this.vbox1.Spacing = 6;
 		// Container child vbox1.Gtk.Box+BoxChild
-		this.UIManager.AddUiFromString ("<ui><toolbar name='toolbar1'><toolitem name='addAction' action='addAction'/><toolitem name='refreshAction' action='refreshAction'/></toolbar></ui>");
+		this.UIManager.AddUiFromString ("<ui><toolbar name='toolbar1'><toolitem name='addAction' action='addAction'/><toolitem name='refreshAction' action='refreshAction'/><toolitem name='deleteAction' action='deleteAction'/></toolbar></ui>");
 		this.toolbar1 = ((global::Gtk.Toolbar)(this.UIManager.GetWidget ("/toolbar1")));
 		this.toolbar1.Name = "toolbar1";
 		this.toolbar1.ShowArrow = false;
@@ -61,5 +64,6 @@ public partial class MainWindow
 		this.DeleteEvent += new global::Gtk.DeleteEventHandler (this.OnDeleteEvent);
 		this.addAction.Activated += new global::System.EventHandler (this.OnAddActionActivated);
 		this.refreshAction.Activated += new global::System.EventHandler (this.OnRefreshActionActivated);
+		this.deleteAction.Activated += new global::System.EventHandler (this.OnDeleteActionActivated);
 	}
 }
